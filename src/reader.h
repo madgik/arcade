f8a76ef50baad43381d2a508fcd00c4a7755817d
johnfouf <johnfouf@gmail.com>
2021-06-08 17:09:27 +0300

    rewrite example test

--- src/reader.h
+++ src/reader.h
@@ -1,28 +1,17 @@
-#include <cstdio>
-#include <cstdlib>
-#include <string>
-#include <vector>
-#include <msgpack.hpp>
 #include <iostream>
 #include <sstream>
 #include <unistd.h>
-#include <map>
 #include <iterator>
 #include <sys/time.h>
 #include <math.h>
 #include <cassert>
 #include <fstream>
-#include <iostream>
 #include <memory>
 #include "snappy.h"
 #include <getopt.h>
 #include <string>
 #include <memory>
-#include <iostream>
-#include <string>
 #include <gtest/gtest.h>
-#include "hps/hps.h"
-#include <algorithm>
 #include <array>
 #include <cmath>
 #include <cstdint>
@@ -31,14 +20,11 @@
 #include <memory>
 #include <vector>
 #include <cmath>
-#include <iostream>
-#include <vector>
-#include "hps/hps.h"
 #include <coroutine>
-
 #include "generator.h"
-using namespace std;
 #include "process.h"
+using namespace std;
+
 
 namespace Arcade{
 int print_columns(char*** &, int, int);
